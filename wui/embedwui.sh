435ef992a00522faf4161ac8b4eea45163fc40e2
Marcus <mrcs.git@mailnull.com>
2016-06-29 09:04:05 +0200

    first install elm packages

--- wui/embedwui.sh
+++ wui/embedwui.sh
@@ -7,7 +7,8 @@ if which eg.sh > /dev/null; then
 	EG=$(which eg.sh)
 fi
 
-$EG elm make "$@"                                                || exit 20
+$EG elm package install --yes                                    || exit 10
+$EG elm make --yes "$@"                                          || exit 20
 # echo "<%! func WriteWuiHtml( w io.Writer ) error %>"  >  wui.ego || exit 30
 cat index.html                                        >  wui.ego || exit 30
 echo                                                 >>  wui.ego || exit 40
